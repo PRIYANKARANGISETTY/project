7f20ce3cd1448822e346f48936d460babc912af4
PRIYANKARANGISETTY <161281072+PRIYANKARANGISETTY@users.noreply.github.com>
2025-12-10 18:44:18 +0530

    footer.js

---
+++ footer.js
@@ -0,0 +1,7 @@
+export default function footer() {
+    return `
+        <footer style="padding: 10px; background: lightgray; margin-top: 20px;">
+            <p>© 2025 Modular App</p>
+        </footer>
+    `;
+}

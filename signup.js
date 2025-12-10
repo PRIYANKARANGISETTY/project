31f8ea32fdba5b083a97ae46e77a223d2f214a10
PRIYANKARANGISETTY <161281072+PRIYANKARANGISETTY@users.noreply.github.com>
2025-12-10 18:45:30 +0530

    signup.js

---
+++ signup.js
@@ -0,0 +1,16 @@
+const form = document.getElementById("signupForm");
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const user = {
+        name: document.getElementById("name").value,
+        email: document.getElementById("email").value,
+        password: document.getElementById("password").value
+    };
+
+    localStorage.setItem("user", JSON.stringify(user));
+
+    alert("Signup successful!");
+    window.location.href = "login.html";
+});
